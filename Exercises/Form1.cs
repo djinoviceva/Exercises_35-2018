ada23f4ba0e5aa66216376e44a9e0e904662baab
LAPTOP-AFT5D8TK\User <jelenadjinovic99@gmail.com>
2020-11-07 13:35:15 +0100

    Povučeni podaci iz baze podataka i smešteni u ListBox.

--- Exercises/Form1.cs
+++ Exercises/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Exercises
 {
@@ -17,5 +19,36 @@ namespace Exercises
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection()) { 
+
+                List<ExerciseResult> list = new List<ExerciseResult>();
+
+            
+            connection.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT  *  FROM  ExerciseResults";
+
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+
+                list.Add(new ExerciseResult(dataReader.GetInt32(0), dataReader.GetString(1), dataReader.GetString(2), dataReader.GetInt32(3)));
+            }
+
+            foreach (ExerciseResult x in list)
+            {
+                listBoxExerciseResults.Items.Add(x);
+            }
+            connection.Close();
+        }
+
     }
 }
+    }
+
